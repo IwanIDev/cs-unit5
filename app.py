3fa125300156875dfbd06de3eb19bcf2d1a748b3
IwanIDev <iwan@iwani.dev>
2024-01-13 18:31:11 +0000

    Added a requirement for Python 3.11

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from ui import App
 import customtkinter
 import logging
 import os
+import sys
 
 if __name__ == "__main__":
     customtkinter.set_default_color_theme("green")
     logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), format="%(asctime)s %(levelname)s %(message)s")
-    app = App()
-    app.mainloop()
+
+    if sys.version_info < (3, 11):
+        logging.error(msg=f"This script requires Python 3.11 or greater.  You are running Python {sys.version_info.major}.{sys.version_info.minor}.")
+    else:
+        app = App()
+        app.mainloop()
+
