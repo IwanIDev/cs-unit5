be4aa159398ab3271eab250a7998b3e3353ad65b
IwanIDev <iwan@iwani.dev>
2024-02-13 14:01:54 +0000

    Fixed isbn checksum.

--- utils/validation.py
+++ utils/validation.py
@@ -1,16 +1,15 @@
-def isbn_check_digit(isbn: int) -> bool:
-    digits = [int(x) for x in str(isbn)]
-    lastDigit = digits.pop()
-    newDigits = []
-    for count, digit in enumerate(digits):
-        place = count + 1
-        if place % 2 == 0:
-            newDigits.append(digit * 3)
+def isbn_checksum(isbn: int) -> bool:
+    # Modified version of https://stackoverflow.com/questions/68587793/making-isbn-10-identifier-in-python
+    isbn_digits = [x for x in str(isbn)]
+    result = 0
+    for index, multiplier in enumerate(reversed(range(1, 11))):
+        char = isbn_digits[index]
+        if char.isalpha() and char == 'X' and not index == 9:
+            return False
+        elif char.isalpha() and not char == 'X':
+            return False
+        elif char.isalpha() and char == 'X' and index == 9:
+            result = result + (10 * multiplier)
         else:
-            newDigits.append(digit)
-    total = 0
-    for digit in newDigits:
-        total = total + digit
-    modulo = total % 10
-    checkDigit = modulo if modulo == 0 else 10 - modulo
-    return lastDigit == checkDigit
+            result = result + (int(char) * multiplier)
+    return not (result % 11)
