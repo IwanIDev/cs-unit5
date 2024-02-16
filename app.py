6f8e16fc48ae388d6a63af33801720245d99c3aa
IwanIDev <iwan@iwani.dev>
2024-02-16 19:46:51 +0000

    More minor changes.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from ui import App
 import logging
 import os
 from PyQt6 import QtWidgets
-import qdarktheme
 from pathlib import Path
 
 
@@ -16,7 +15,6 @@ def get_stylesheet(stylesheet: Path):
 if __name__ == "__main__":
     logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), format="%(asctime)s %(levelname)s %(message)s")
     app = QtWidgets.QApplication(sys.argv)
-    #qdarktheme.setup_theme()
     window = App()
     window.show()
     app.exec()
--- user_manager/__init__.py
+++ user_manager/__init__.py
@@ -1,3 +1,4 @@
 from .logins import register_user, login_user, LoginUserException, RegisterUserException
 from .manage_users import delete_user, get_all_users
 from .user import User
+from .exceptions import *
